5a9e416012cd8aa7a7107fff32601d857f861b50
TrungTin <trungtinst258@gmail.com>
2022-03-22 20:18:23 +0700

    Lab05_2

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,13 +1,15 @@
 import { Component } from 'react/cjs/react.production.min';
 import Main from './components/MainComponents';
-
+import { BrowserRouter } from 'react-router-dom';
 class App extends Component {
 
   render () {
     return (
+      <BrowserRouter>
       <div >
           <Main/>
-      </div>
+       </div>
+      </BrowserRouter>
     );
   }
 }
--- reactapp/src/components/MainComponents.js
+++ reactapp/src/components/MainComponents.js
@@ -1,9 +1,11 @@
 import { Component } from 'react/cjs/react.production.min';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetails from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
   constructor (props) {
@@ -11,19 +13,23 @@ class Main extends Component {
 
     this.state = {
         dishes: DISHES,
-        selectedDish:null
-    };
+      };
     }
-    onDishSelect (dishId){
-        this.setState({selectedDish: dishId}); //tao ra trang thai mới cho selectedish null thành cái mình đang chọn
-            
-     }
-  render () {
+  
+  render() {
+    const HomePage = () => {
+      return (
+          <Home />
+        )
+    }
     return (
       <div className='App'>
             <Header />
-            <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
-            <DishDetails dish={this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish)[0]} />
+            <Switch>
+                <Route path="/home" component={ HomePage }/>
+                 <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                <Redirect to ="/home" />
+            </Switch>
             <Footer />
       </div>
     );
