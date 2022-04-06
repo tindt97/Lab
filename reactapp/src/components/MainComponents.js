1f1b4872d5ffd2829b49c415e2411664621b0f2e
TrungTin <trungtinst258@gmail.com>
2022-04-06 20:53:42 +0700

    ex2

--- reactapp/src/components/MainComponents.js
+++ reactapp/src/components/MainComponents.js
@@ -2,6 +2,7 @@ import { Component } from 'react/cjs/react.production.min';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetails from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
@@ -52,6 +53,7 @@ class Main extends Component {
                 <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
                 <Route path="/menu/:dishId" component={DishWithId}/>
                 <Route exact path="/contactus" component={Contact} />
+                <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
                 <Redirect to ="/home" />
             </Switch>
             <Footer />
